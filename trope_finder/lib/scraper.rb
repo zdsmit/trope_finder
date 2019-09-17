1a7cca7981dd6d9b5c6f35b6848625f7b679bef6
Zach  Smith <zdsmith001@gmail.com>
2019-09-17 04:15:57 +0000

    installed pry and nokogiri, started to write scraper method

--- trope_finder/lib/scraper.rb
+++ trope_finder/lib/scraper.rb
@@ -4,6 +4,14 @@ require 'nokogiri'
 
 class Scraper
   
-  
+  def trope_list
+    trope_index = Nokogiri::HTML(open("https://tvtropes.org/pmwiki/pmwiki.php/Main/Plots"))
+    binding.pry
+  end
   
 end
+
+Scraper.new.trope_list
+
+#plot index url: https://tvtropes.org/pmwiki/pmwiki.php/Main/Plots
+#note 9/16: not sure if nokogiri is functioning right. experiment with different urls to make sure class is scraping data correctly. also consult past lessons to make sure syntax is ok for nokogiri call

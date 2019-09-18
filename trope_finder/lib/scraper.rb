0599ebd6d611b11412f9e924906de76b5a8cd60a
Zach  Smith <zdsmith001@gmail.com>
2019-09-18 21:18:10 +0000

    changed 'trope_index' to 'trope_html', started creating trope index iterator

--- trope_finder/lib/scraper.rb
+++ trope_finder/lib/scraper.rb
@@ -5,7 +5,10 @@ require 'nokogiri'
 class Scraper
   
   def trope_list
-    trope_index = Nokogiri::HTML(open("https://tvtropes.org/pmwiki/pmwiki.php/Main/Plots"))
+    trope_html = Nokogiri::HTML(open("https://tvtropes.org/pmwiki/pmwiki.php/Main/Plots"))
+    trope_index = trope_html.css("div.article-content.retro-folders ul")[1].children
+    trope_index.each do |trope|
+    end
   end
   
 end
@@ -13,7 +16,7 @@ end
 Scraper.new.trope_list
 
 #plot index url: https://tvtropes.org/pmwiki/pmwiki.php/Main/Plots
-#note 9/17: still attempting to find the right css selector for scraping. the closest i've gotten is as follows:       trope_index.css("div.article-content.retro-folders ul")[1]
-#full trope index: trope_index.css("div.article-content.retro-folders ul")[1].children
-#trope names: trope_index.css("div.article-content.retro-folders ul")[1].children.first.children[1].text
-#trope page urls: trope_index.css("div.article-content.retro-folders ul")[1].children.first.children[1].attr("href
+#note 9/17:
+#full trope index: trope_html.css("div.article-content.retro-folders ul")[1].children
+#trope names: trope_html.css("div.article-content.retro-folders ul")[1].children.first.children[1].text
+#trope page urls: trope_html.css("div.article-content.retro-folders ul")[1].children.first.children[1].attr("href

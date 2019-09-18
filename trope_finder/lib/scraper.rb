394bf0d535d9e23bf2ed4196fb6b42bec160c499
Zach  Smith <zdsmith001@gmail.com>
2019-09-18 21:00:08 +0000

    found nokogiri selectors for the full trope list, trope names, and trope page urls

--- trope_finder/lib/scraper.rb
+++ trope_finder/lib/scraper.rb
@@ -6,7 +6,6 @@ class Scraper
   
   def trope_list
     trope_index = Nokogiri::HTML(open("https://tvtropes.org/pmwiki/pmwiki.php/Main/Plots"))
-    binding.pry
   end
   
 end
@@ -15,4 +14,6 @@ Scraper.new.trope_list
 
 #plot index url: https://tvtropes.org/pmwiki/pmwiki.php/Main/Plots
 #note 9/17: still attempting to find the right css selector for scraping. the closest i've gotten is as follows:       trope_index.css("div.article-content.retro-folders ul")[1]
-# continue experimenting to find the best selector
+#full trope index: trope_index.css("div.article-content.retro-folders ul")[1].children
+#trope names: trope_index.css("div.article-content.retro-folders ul")[1].children.first.children[1].text
+#trope page urls: trope_index.css("div.article-content.retro-folders ul")[1].children.first.children[1].attr("href

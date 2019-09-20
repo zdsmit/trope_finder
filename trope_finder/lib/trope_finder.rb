b4883bf601474a680603af3bc2771aede1d4ac16
Zach  Smith <zdsmith001@gmail.com>
2019-09-20 21:18:38 +0000

    improved user interface by starting with a yes/no prompt

--- trope_finder/lib/trope_finder.rb
+++ trope_finder/lib/trope_finder.rb
@@ -9,16 +9,24 @@ class TropeFinder
   end
   
   def user_interface
-    puts "Welcome to your trope index! Whether you're a big fan of TV, books, movies, or something else, you have definitely seen a great variety of tropes and cliches. This will allow you to have some fun exploring them!"
-    puts "Below you will find a list of tropes. Pick one by number to get more information about it!"
-    @index.trope_list(@url)
-    input = gets.strip
-    @index.trope_list(@url).detect do |trope_entry|
-      if trope_entry.include?(input)
-        puts trope_entry
+    puts "Welcome to the trope index! Whether you're a big fan of TV, books, movies, or something else, you have definitely seen a great variety of tropes and cliches. This will allow you to have some fun exploring them!"
+    puts "You can get more information about your favorite tropes here. Would you like to see a list of tropes? [y/n]:"
+    answer = gets.strip
+    answer = answer.to_s
+    if answer == "y"
+      @index.trope_list(@url)
+      input = gets.strip
+      @index.trope_list(@url).detect do |trope_entry|
+        if trope_entry.include?(input)
+          puts trope_entry
+        end
       end
+    elsif answer == "n"
+      puts "Thank you for using the Trope Finder. Please have a great day."
     end
   end
+  
+  TropeFinder.new.user_interface
   
   # note 9/20: the user_interface isn't working yet. it needs to identify the number of the trope from the list and return that entry based on user input. also, may want to consider changing welcome message setup due to large size of trope list
   

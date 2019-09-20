f7f05bc64c11d82e339fe71c9e9f07cbabd0976d
Zach  Smith <zdsmith001@gmail.com>
2019-09-20 16:54:00 +0000

    added each_with_index to Scraper.trope_list method, required scraper file in trope_finder.rb

--- trope_finder/lib/scraper.rb
+++ trope_finder/lib/scraper.rb
@@ -8,13 +8,14 @@ class Scraper
   def trope_list(url)
     trope_html = Nokogiri::HTML(open(url))
     trope_index = trope_html.css("div.article-content.retro-folders ul")[1].children
-    trope_index.each do |trope|
+    trope_index.each_with_index do |trope, number|
+      number += 1
       url = trope.css('a')
       trope_url = url.map {|element| element["href"]}.compact
       text = trope.text
       divide = text.split(":")
       trope_name = divide[0]
-      puts "#{trope_name}: #{trope_url}"
+      puts "#{number}. #{trope_name}: #{trope_url}"
     end
   end
   
--- trope_finder/lib/trope_finder.rb
+++ trope_finder/lib/trope_finder.rb
@@ -1,11 +1,17 @@
+require_relative './scraper.rb'
+
 class TropeFinder
-  attr_accessor :index
+  attr_accessor :index, :url
   
   def initialize(trope_url="https://tvtropes.org/pmwiki/pmwiki.php/Main/Plots")
     @index = Scraper.new
+    @url = trope_url
   end
   
   def user_interface
+    puts "Welcome to your trope index! Whether you're a big fan of TV, books, movies, or something else, you have definitely seen a great variety of tropes and cliches. This will allow you to have some fun exploring them!"
+    puts "Below you will find a list of tropes. Pick one by number to get more information about it!"
+    @index.trope_list(@url)
   end
   
 end

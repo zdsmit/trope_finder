b60ae7db947c78c8cd979d6f4380e4e7831a1ae4
Zach  Smith <zdsmith001@gmail.com>
2019-09-20 17:08:51 +0000

    started constructing user_interface iterator

--- trope_finder/lib/trope_finder.rb
+++ trope_finder/lib/trope_finder.rb
@@ -12,6 +12,14 @@ class TropeFinder
     puts "Welcome to your trope index! Whether you're a big fan of TV, books, movies, or something else, you have definitely seen a great variety of tropes and cliches. This will allow you to have some fun exploring them!"
     puts "Below you will find a list of tropes. Pick one by number to get more information about it!"
     @index.trope_list(@url)
+    input = gets.strip
+    @index.trope_list(@url).detect do |trope_entry|
+      if trope_entry.include?(input)
+        puts trope_entry
+      end
+    end
   end
+  
+  # note 9/20: the user_interface isn't working yet. it needs to identify the number of the trope from the list and return that entry based on user input. also, may want to consider changing welcome message setup due to large size of trope list
   
 end
